8481dd2a5c49215118c5b1279f77be1e1e6a23fb
Khalid Alshamsan <Khalid@MacBookPro.local>
2018-09-24 00:30:56 -0700

    First commit

---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour {
+
+    public void Quiting()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+    public float speed;
+    public Text counterText, winText;
+    private int counter;
+    private Rigidbody playerRb;
+
+
+
+    void Start()
+    { playerRb = GetComponent<Rigidbody>();
+        audioSource.clip = audioClip;
+        counter = 0; 
+        winText.text = "";
+        CounterTextUpdate();
+    }
+
+    void FixedUpdate () 
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        playerRb.AddForce(playerMovement * speed);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.gameObject.CompareTag("collect"))
+        {
+            other.gameObject.SetActive(false);
+            audioSource.Play();
+            counter = ++counter;
+            CounterTextUpdate();
+        }
+    }
+
+    void CounterTextUpdate()
+    {
+        counterText.text = "Points: " + counter.ToString();
+        if (counter >= 10) { winText.text = "You Won!!"; }
+
+    }
+}
